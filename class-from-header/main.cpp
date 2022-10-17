14bf04e1a3a6e0202f1886e6dbbad3e81ee36ca9
checkthistape <106949307+checkthistape@users.noreply.github.com>
2022-10-17 22:32:58 +0200

    Create main.cpp

---
+++ class-from-header/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "header.h"
+
+
+int main()
+{
+	Person Man(34, "John", "skill", 'y', 23.5);
+    
+
+	/*Person Man;
+	Man.SetAge(12);
+	Man.SetName("John");
+	Man.SetSkill("programmer");
+	Man.SetEyeCol('y');
+	Man.SetFootSize(43.2);
+
+
+	cout << Man.GetAge() << "\t";
+	cout << Man.GetName() << "\t";
+	cout << Man.GetSkill() << "\t";
+	cout << Man.GetEyeCol() << "\t";
+	cout << Man.GetFootSize() << "\t";*/
+
+
+    /*cout << Man. << endl;*/
+}
