6a136f6384e21a87e61d512d1d4aa2415c9b765e
checkthistape <106949307+checkthistape@users.noreply.github.com>
2022-10-17 22:29:57 +0200

    Create main.cpp

---
+++ car-project/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<dos.h> 
+#include <windows.h>
+#include <time.h>
+#include "play.cpp"
+//#include "start-menu.cpp"
+
+using namespace std; 
+ 
+
+void play();
+void startmenu();
+
+
+int main(){
+	startmenu();
+	return 0;
+	
+}
